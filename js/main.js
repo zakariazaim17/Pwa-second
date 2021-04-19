6bcb3ea8e3046583e10f135996a5bcaa77954954
zakaria ziouziou <zakariaziouziou@gmail.com>
2021-04-19 10:53:01 +0300

    fixing db path

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.addEventListener("load", async () => {
   const ul = document.querySelector("ul");
   const rfrsh = document.querySelector("#refresh");
   const form = document.querySelector("form");
-  const username = "Nebi";
+  const username = "zakaria";
   const greeting = form.elements.greeting;
   console.log("hello");
 
